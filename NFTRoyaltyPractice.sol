ef9eb65fa7515a45860d88d5b808ca7884440d80
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-07-18 10:48:08 -0700

    added customizablity: deployer can choose max supply and max supply per token id as well as set the payments address

--- NFTRoyaltyPractice.sol
+++ NFTRoyaltyPractice.sol
@@ -18,12 +18,12 @@ contract NFTRoyaltyPractice is ERC1155, Ownable {
     // constants
 
     uint256 public immutable mintPrice;
-    uint256 public totalSupply;
-    uint256 public immutable maxSupply
+    uint256 public immutable maxSupply;
+    uint256 public immutable maxPerTokenId;
     bool public mintEnabled;
 
     // uris for nft metadata
-    mapping(uint256 => string) private _uris;
+    mapping(uint256 => string) private uris;
 
     // whitelisted addresses and how many tokens they can purchase
     mapping(address => uint) public whitelistedAddresses;
@@ -31,34 +31,47 @@ contract NFTRoyaltyPractice is ERC1155, Ownable {
     // tokenID mapped to date whitelist option is valid
     mapping(uint256 => uint) public validUntil;
 
+    // tokenId => total supply
+    mapping(uint256 => uint256) public totalSupply;
+
+    // tracks the number of tokenId's minted per wallet: address => (tokenId => total minted)
+    mapping(address => mapping(uint256 => uint256)) public tokenIdMints;
+
     // constructor
-    constructor() payable ERC1155("NFTRoyaltyPractice") {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _maxPerTokenId,
+        uint256 _maxSupply,
+        address _payments
+    ) payable ERC1155(_name, _symbol) {
         // initialize variables
         mintPrice = 0.01 ether;
-        totalSupply = 0;
-        maxSupply = 
+        maxSupply = _maxSupply;
+        maxPerTokenId = _maxPerTokenId;
+        payments = payable(_payments);
     }
 
     // functions
 
     // enables minting
-    function setMintEnabled(bool mintEnabled_) external onlyOwner {
-        mintEnabled = mintEnabled_;
+    function setMintEnabled(bool _mintEnabled) external onlyOwner {
+        mintEnabled = _mintEnabled;
     }
 
     // returns uri of a given tokenId
-    function uri(uint256 tokenId) override public view returns (string memory) {
-        return(_uris[tokenId]);
+    function uri(uint256 _tokenId) override public view returns (string memory) {
+        return(uris[_tokenId]);
     }
 
     // sets the uri for each tokenID and stores in _uris mapping
     function setTokenUri(uint256 _tokenId, string memory _uri) public onlyOwner {
-        _uris[_tokenId] = _uri;
+        uris[_tokenId] = _uri;
     }
 
     // sets date for whitelisted addresses to mint by
-    function setValidUntil(uint256 _tokenId, uint daysNo) public onlyOwner {
-        validUntil[_tokenId] = block.timestamp + (daysNo * 1 days);
+    function setValidUntil(uint256 _tokenId, uint _daysNo) public onlyOwner {
+        validUntil[_tokenId] = block.timestamp + (_daysNo * 1 days);
     }
 
     // returns bool whether whitelisted address can still mint
@@ -67,11 +80,29 @@ contract NFTRoyaltyPractice is ERC1155, Ownable {
     }
 
     // public mint
-    function mint(address recipient, uint256 tokenId, uint256 amount) public payable {
+    function mint(address _recipient, uint256 _tokenId, uint256 _amount) public payable {
         require(mintEnabled, "Minting has not been enabled.");
-        require(msg.value == amount * mintPrice, "Incorrect mint value.");
-        require(totalSupply + amount <= maxSupply, "Sorry, we're sold out.")
+        require(msg.value == _amount * mintPrice, "Incorrect mint value.");
+        require(totalSupply[_tokenId] + amount <= maxSupply, "Sorry, you have exceeded the supply.");
+        require(tokenIdMints[msg.sender][_tokenId] + _amount == maxPerTokenId);
 
+        tokenIdMints[msg.sender][_tokenId] += _amount;
+        
+
+    }
+
+    // allows owner to choose an address and how many nfts that address can mint
+    function updateWhitelist(address _addr, uint _amount) public onlyOwner {
+        whitelistAddresses[_addr] = _amount;
+    }
+
+    function whitelistMint(address _recipient, uint256 _tokenId, uint256 _amount) public {
+        require(_amount >= 1, "please enter a valid number");
+        require(whitelistedAddresses[msg.sender] >= _amount, "This address has not been whitelisted.");
+        require(isValid(_tokenId) == true);
+
+        _mint(_recipient, _tokenId, _amount, "");
+        whitelistedAddresses[msg.sender] -= _amount;
     }
 
 
